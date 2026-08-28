4c43cc8102c2bfccb27dedb5a5fcf4d9d29e0b55
Shine13102544 <690426027005@bru.ac.th>
2026-08-28 22:23:42 +0700

    fix: ignore dotenv on vercel environment

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
+// โหลด .env เฉพาะเมื่อรันในเครื่อง (Local)
+if (process.env.NODE_ENV !== 'production' && !process.env.VERCEL) {
+  const path = require('path');
+  require('dotenv').config({ path: path.resolve(__dirname, '../backend/.env') });
+}
+
 const app = require('../backend/server.js');
 
 module.exports = app;
